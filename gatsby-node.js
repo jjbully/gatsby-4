0070c0a082a898ace3d1fe79b6148376dbce3f32
Jeffrey <jeflcc.io@gmail.com>
2019-08-25 11:23:45 +0800

    0703____****traverse the “node graph” to its parent File node****

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
 //This onCreateNode function will be called by Gatsby whenever a new node is created (or updated).
 
-exports.onCreateNode = ({ node }) => {
+exports.onCreateNode = ({ node, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
-    console.log(node.internal.type)
+    console.log('jeffrey')
+    const fileNode = getNode(node.parent)
+    console.log(`\n`, fileNode.relativePath)
   }
 }
