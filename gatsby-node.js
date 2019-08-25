134778d2c37f00a9fab5c12e229e9feebf9465b4
Jeffrey <jeflcc.io@gmail.com>
2019-08-25 11:28:36 +0800

    0704 - ****Function to create slug ****

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,11 @@
 //This onCreateNode function will be called by Gatsby whenever a new node is created (or updated).
 
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
 exports.onCreateNode = ({ node, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
     console.log('jeffrey')
-    const fileNode = getNode(node.parent)
-    console.log(`\n`, fileNode.relativePath)
+    //function to create slug
+    console.log(createFilePath({ node, getNode, basePath: `pages` }))
   }
 }
