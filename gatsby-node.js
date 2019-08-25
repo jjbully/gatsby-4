523b011218f7d19e8957fd1409c496728efee218
Jeffrey <jeflcc.io@gmail.com>
2019-08-25 11:15:26 +0800

    0702____****run a callback when a node is create in the Gatsby node system****

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,7 @@
 //This onCreateNode function will be called by Gatsby whenever a new node is created (or updated).
 
 exports.onCreateNode = ({ node }) => {
-  console.log(node.internal.type)
+  if (node.internal.type === `MarkdownRemark`) {
+    console.log(node.internal.type)
+  }
 }
